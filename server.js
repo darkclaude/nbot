2db2d859396dd6e3f63f28ca6ff64995393b25ae
Hamid <darkclaudevfx@gmail.com>
2015-11-02 17:29:47 -0800

    g

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-﻿var Client = require('node-rest-client').Client;
- 
-client = new Client();
+﻿var rest = require('restler');
+
  var c=0;
 var d = 6758;
 var f = 90;
+var token= "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NjJhNzJkNGJmYTYxMTY3YTNkMmRiZWUiLCJsb2NhbCI6eyJwYXNzd29yZCI6IiQyYSQwOSRuV3NuSEl6UGVHdnlnYlBmWTUzeHhPTXZIU0g3T2xWYVM3d3FNdmxvek5oUmhjUEZQRHJkeSIsInVzZXJuYW1lIjoiYXBpIn19.jgploZQxiycZAC9nBFz_SkH6s1ou0WO6cPQN1F0Q59g";
 var checkst = setInterval(function(){
 c=c+1;
 d=d-2;
@@ -17,14 +17,15 @@ d=7000;
 if(c>=250){
 c=10;
 }
-var dat = c.toString()+"/"+d.toString()+"/"+f.toString();
-console.log(dat);
-	client.get("http://nodejs-ninjax.rhcloud.com/api/node/"+dat+"/ndns?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NjJhNzJkNGJmYTYxMTY3YTNkMmRiZWUiLCJsb2NhbCI6eyJwYXNzd29yZCI6IiQyYSQwOSRuV3NuSEl6UGVHdnlnYlBmWTUzeHhPTXZIU0g3T2xWYVM3d3FNdmxvek5oUmhjUEZQRHJkeSIsInVzZXJuYW1lIjoiYXBpIn19.jgploZQxiycZAC9nBFz_SkH6s1ou0WO6cPQN9g1F0Q5", function(data, response){
-            // parsed response body as js object 
-            console.log(data);
-            // raw response 
-            console.log(response);
-        });
-},3000);
+var dat = f.toString()+"/"+d.toString()+"/"+c.toString();
+rest.get("http://nodejs-ninjax.rhcloud.com/api/node/"+dat+"/ndns?token="+token).on('complete', function(result) {
+  if (result instanceof Error) {
+    console.log('Error:', result.message);
+    this.retry(5000); // try again after 5 sec
+  } else {
+    console.log(result);
+  }
+});
+},2000);
 
  
